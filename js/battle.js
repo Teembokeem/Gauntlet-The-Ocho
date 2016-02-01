c01b1c4680e2001d8cac95d5ed96508130cdac45
myunghoon kim <myungtkim@gmail.com>
2016-02-01 10:02:06 -0800

    takes out some redundant grey text. realigns some functions for better readability.

--- js/battle.js
+++ js/battle.js
@@ -1,7 +1,7 @@
 console.log("POKEBALLS READY");
 
 //GLOBAL VARIABLES
-  //Mon Properties
+  //Avatar Properties
     var Avatar = function(name, health, attackName, power) {
       this.name = name;
       this.maxhealth = health;
@@ -48,10 +48,10 @@ console.log("POKEBALLS READY");
 
     var onePunchPhil = new enemyAvatar("One Punch Phil", 100000, "one-punch", "mercy-punch", "huehue...weakling....");
 
-    var pokemonList = [pikaFreakingChu, dragonite];
-    var activeAvatar = pokemonList[0]; //needs to grab whoever.
+    var avatarList = [pikaFreakingChu, dragonite];
+    var activeAvatar = avatarList[0]; //needs to grab whoever.
     var activeEnemyAvatar = onePunchPhil;
-    var usablePokemonList = pokemonList;
+    var usableAvatarList = avatarList;
 
      //needs to grab whoever.
 
@@ -81,96 +81,77 @@ console.log("POKEBALLS READY");
 
     var itemList = [regularPotion, attackUp];
 
-var battle = function() {
-    console.log("A wild " + activeEnemyAvatar.name + " has appeared!");
-    console.log(pokemonList[0].name + " is up!");
-};
-
-//1. tick-based, animations running, waiting for user response
-
-//Master fnc: while player hp > 1, and enemy hp >1
-                //run player fnc
-                //run enemy fnc
-              // if player hp <1,
-
-//MASTER attack fnc (click/pressing enter): NEEDS FUNCTION
-
-  var attack = function() {       // NEEDS EVENT LISTENER ON ATTACK NAME'S CLICK
-    //a. damage calculation fnc
-    activeAvatar.battle(activeEnemyAvatar);
-    //b. "declares attack in text"
-    //c. "attack animation"
-
-    //d. "response animation"
-
-    //e. "health bar goes down"
-
-    //f. "attack effectiveness/oneliner in text"
-    //g1. enemy's turn fnc (uses while hp>0, else...)
-    if (activeEnemyAvatar.currenthealth > 0) {
-      enemyAttack(activeEnemyAvatar, activeAvatar);
-    } else {
-      //endbattle fnc.
-      console.log('you win!');                        //END BATTLE REWARDS, LOADING SCREEN, WORLD SCREEN
-    }
+//BATTLE SCREEN FUNCTIONS
+  var battle = function() {
+      console.log("A wild " + activeEnemyAvatar.name + " has appeared!");
+      console.log(avatarList[0].name + " is up!");
   };
-var updateUsablePokemon = function() {                    //NEEDS TO RUN WITH COFFEE POKEMON CENTER.
-  for (i=0; i<usablePokemonList.length; i++) {
-    if (usablePokemonList[i].currenthealth <= 0) {
-      usablePokemonList.splice(usablePokemonList[i], 1);
-    }
-  }
-};
 
-  var enemyAttack = function() {                      //LEAVE IT LIKE THIS BECAUSE OTHERWISE RUNS LOOP.
-    activeEnemyAvatar.battle(activeAvatar);
+  //1. tick-based, animations running, waiting for user response
 
-    updateUsablePokemon();
-    if (activeAvatar.currenthealth <= 0 && usablePokemonList != []) {   //THIS LOGIC NEEDS TO HASH OUT.
-      var switchPrompt = prompt("Do you want to switch out?");  // NEEDS TO TURN TO TEXT ASKING SWITCH. AND YES OR NO KEYUP OR DOWN + ENTER.
-      if (switchPrompt = "yes") {
-        switchOut();
+  //I. Player Turn Function
+    var attack = function() {       // NEEDS EVENT LISTENER ON ATTACK NAME'S CLICK
+      //a. damage calculation fnc
+      activeAvatar.battle(activeEnemyAvatar);
+      //b. "declares attack in text"
+
+      //c. "attack animation"
+
+      //d. "response animation"
+
+      //e. "health bar goes down"
+
+      //f. "attack effectiveness/oneliner in text"
+
+      //g1. enemy's turn fnc (uses while hp>0, else...)
+      if (activeEnemyAvatar.currenthealth > 0) {
+        enemyAttack(activeEnemyAvatar, activeAvatar);
       } else {
-        console.log('leaving battle....');
-        console.log(activeEnemyAvatar.oneLiner);               //END BATTLE PUNISHMENTS, LOADING SCREEN, WORLD SCREEN/STORY LOADING FOR WAKING UP FROM DYING.);
+        //endbattle fnc.
+        console.log('you win!');                        //END BATTLE REWARDS, LOADING SCREEN, WORLD SCREEN
       }
-    }
-  };
+    };
+    var updateUsableAvatar = function() {                    //NEEDS TO RUN WITH COFFEE Avatar CENTER.
+      for (i=0; i<usableAvatarList.length; i++) {
+        if (usableAvatarList[i].currenthealth <= 0) {
+          usableAvatarList.splice(usableAvatarList[i], 1);
+        }
+      }
+    };
 
+    var enemyAttack = function() {                      //LEAVE IT LIKE THIS BECAUSE OTHERWISE RUNS LOOP.
+      activeEnemyAvatar.battle(activeAvatar);
 
-//1. Player turn fnc
-
-  //I. Player Attack func
-    var attackselected = function() {
-      attack(activeAvatar, activeEnemyAvatar);
+      updateUsableAvatar();
+      if (activeAvatar.currenthealth <= 0 && usableAvatarList != []) {   //THIS LOGIC NEEDS TO HASH OUT.
+        var switchPrompt = prompt("Do you want to switch out?");  // NEEDS TO TURN TO TEXT ASKING SWITCH. AND YES OR NO KEYUP OR DOWN + ENTER.
+        if (switchPrompt = "yes") {
+          switchOut();
+        } else {
+          console.log('leaving battle....');
+          console.log(activeEnemyAvatar.oneLiner);               //END BATTLE PUNISHMENTS, LOADING SCREEN, WORLD SCREEN/STORY LOADING FOR WAKING UP FROM DYING.);
+        }
+      }
     };
   //II. item menu chosen (takes a turn): NEEDS FUNCTION
 
- /* var queryItem = function(itemList[0]) {
-    if (activeAvatar.currenthealth === activeAvatar.maxhealth) {
-      console.log('cannot use potion. already at max health');
-    } else {
-      useItem();
-    }
-  };*/
+    var useItem = function(a) {
+      //a. item menu pops up
 
-  var useItem = function(a) {
-    //a. item menu pops up
+      //b. click on item.
 
-    //b. click on item.
+      //c. close item menu
 
-    //c. close item menu
-
-    //d. item effect calc fnc
-    itemList[a].effect(activeAvatar);
-    //e. "declare item use"
-    console.log(activeAvatar.name + " used " + itemList.name);
-    //f. "item use animation"
-    //g. "item effect animation/health bar/status icon"
-    //h. item effectiveness in text
-    console.log(itemList.name + " " + this.description + itemList.power + " health. Sitting at " + activeAvatar.currenthealth + " health.");
-    //i. enemy turn fnc(uses while hp>0)
-  };
+      //d. item effect calc fnc
+      itemList[a].effect(activeAvatar);
+      //e. "declare item use"
+      console.log(activeAvatar.name + " used " + itemList.name);
+      //f. "item use animation"
+      //g. "item effect animation/health bar/status icon"
+      //h. item effectiveness in text
+      console.log(itemList.name + " " + this.description + itemList.power + " health. Sitting at " + activeAvatar.currenthealth + " health.");
+      //i. enemy turn fnc(uses while hp>0)
+    };
 
 
 
@@ -185,11 +166,11 @@ var updateUsablePokemon = function() {                    //NEEDS TO RUN WITH CO
     //h. keith one-liner
     //i. enemy turn fnc (uses while hp>0)
 
-  var switchOut = function() {                                                        //NEEDS TO TURN TO CLICK EVENT ON POKEMON UL LIST.
-      for (i=0; i <usablePokemonList.length; i++) {                                         //THIS NEEDS TO CHANGE TO UNSELECTABLE PANEL FOR FAINTED POKEMON.
-        var selectSwitchAvatar = prompt("Switch to " + usablePokemonList[i].name + "?");  //NEEDS TEXT ASKING FOR SWITCH CONFIRM TO VIABLE AVATAR.
+  var switchOut = function() {                                                        //NEEDS TO TURN TO CLICK EVENT ON Avatar UL LIST.
+      for (i=0; i <usableAvatarList.length; i++) {                                         //THIS NEEDS TO CHANGE TO UNSELECTABLE PANEL FOR FAINTED Avatar.
+        var selectSwitchAvatar = prompt("Switch to " + usableAvatarList[i].name + "?");  //NEEDS TEXT ASKING FOR SWITCH CONFIRM TO VIABLE Avatar.
         if (selectSwitchAvatar = "yes") {
-          activeAvatar = usablePokemonList[i];
+          activeAvatar = usableAvatarList[i];
           console.log('Go!' + activeAvatar.name + "!");
           break;
         } else {
@@ -200,19 +181,19 @@ var updateUsablePokemon = function() {                    //NEEDS TO RUN WITH CO
   };
 
 
-//2. Enemy turn fnc:
-  //I. attack (random if more than one attack)
+  //2. Enemy turn fnc:
+    //I. attack (random if more than one attack)
 
-    //a. damage calculation fnc
-    //b. "declares attack in text"
-    //c. "attack animation"
-    //d. "response animation"
-    //e. "health bar reacts"
-    //f. "attack effectiveness/oneliner in text"
-    //g1. player turn fnc. (uses while hp > 0, else...)
-    //g2. end battle fnc.
+      //a. damage calculation fnc
+      //b. "declares attack in text"
+      //c. "attack animation"
+      //d. "response animation"
+      //e. "health bar reacts"
+      //f. "attack effectiveness/oneliner in text"
+      //g1. player turn fnc. (uses while hp > 0, else...)
+      //g2. end battle fnc.
 
-  //II.
+    //II.
 
 
 
