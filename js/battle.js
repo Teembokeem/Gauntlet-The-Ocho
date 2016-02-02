720196af856086a69c915fbc08ed9c172f6ef6ed
myunghoon kim <myungtkim@gmail.com>
2016-02-02 15:48:58 -0800

    adds new assets. abandoms tile world into map.js. adds core functionality to fight decision, including jquery.

--- js/battle.js
+++ js/battle.js
@@ -1,18 +1,21 @@
 console.log("POKEBALLS READY");
 
 //GLOBAL VARIABLES
+  //Jquery Vars
+
   //Avatar Properties
-    var Avatar = function(name, health, attackName, power) {
+    var Avatar = function(name, health, attack1, power1, attack2, power2, attack3, power3, attack4, power4) {
       this.name = name;
       this.maxhealth = health;
       this.currenthealth = health;
-      this.attackName = attackName;
-      this.power = power;
-      this.battle = function(enemy) {
-        console.log(this.name + " attacks with " + this.attackName + ", which does " + this.power + " damage.");
-        console.log(enemy.name + " was dealt " + this.power + "damage.");
-        enemy.currenthealth -= this.power;
-        console.log(enemy.name + " has " + enemy.currenthealth + " health left.");
+      this.attackNames = [
+        attack1, attack2, attack3, attack4
+      ];
+      this.attackPowers = [
+        power1, power2, power3, power4
+      ];
+      this.battle = function(power) {
+        activeEnemyAvatar.currenthealth -= power;
       }
     };
 
@@ -42,18 +45,16 @@ console.log("POKEBALLS READY");
       }
     }
 
-    var pikaFreakingChu = new Avatar("pika FRICKIN CHU", 1000, "Thunder", 100);
+    var pikaFreakingChu = new Avatar("pika FRICKIN CHU", 1000, "Thunder", 100, "ThunderBolt", 200, "iron tail", 500, "Electroball", 1000);
 
     var dragonite = new Avatar("big fucking dragon", 10000, "Fus Roh Dah", 4000);
 
     var onePunchPhil = new enemyAvatar("One Punch Phil", 100000, "one-punch", "mercy-punch", "huehue...weakling....");
 
     var avatarList = [pikaFreakingChu, dragonite];
-    var activeAvatar = avatarList[0]; //needs to grab whoever.
+    var activeAvatar = avatarList[0];        //needs to grab whoever.
     var activeEnemyAvatar = onePunchPhil;
-    var usableAvatarList = avatarList;
-
-     //needs to grab whoever.
+    var usableAvatarList = avatarList;       //needs to grab whoever.
 
 
   //Item Properties
@@ -81,18 +82,62 @@ console.log("POKEBALLS READY");
 
     var itemList = [regularPotion, attackUp];
 
-//BATTLE SCREEN FUNCTIONS
-  var battle = function() {
-      console.log("A wild " + activeEnemyAvatar.name + " has appeared!");
-      console.log(avatarList[0].name + " is up!");
+
+                                //==============\\
+                                //FIGHT FUNCTION\\
+                                //==============\\
+//after rendering battle scene.
+
+var $worldMap = $('#world_map');
+var $battleBoard = $('#battle_board');
+var $loadingScreen = $('#loading_Screen');
+var $fightTextBox = $('#fight');
+var $itemTextBox = $('#item');
+var $famTextBox = $('#fam');
+var $boneOutTextBox = $('#bone_Out');
+var $battleText = $('battle_Text');
+var $attack1Box = $('#attack1');
+var $attack2Box = $('#attack2');
+var $attack3Box = $('#attack3');
+var $attack4Box = $('#attack4');
+var $attackBoxArray = [$attack1Box, $attack2Box, $attack3Box, $attack4Box];
+
+//Render active Avatar Attacks in Context to attack boxes. NEEDS TO HAPPEN ON EVENT SWITCH/AVATAR DEATH.
+var renderActiveAttacks = function() {
+  for(i=0;i<4;i++) {
+    $attackBoxArray[i].text(activeAvatar.attackNames[i])
+                      .css({'color':'white', 'font-size': '35px',
+                            'position': 'relative',
+                            'display': 'none',
+                            'float':'left'
+                          });
   };
+};
 
-  //1. tick-based, animations running, waiting for user response
+//Clicking Fight brings up attack box.
+$fightTextBox.on("click", function() {
+  $fightTextBox.fadeOut(200);
+  $itemTextBox.fadeOut(300);
+  $famTextBox.fadeOut(400);
+  $boneOutTextBox.fadeOut(500);
+  setTimeout(function() {
+    $attack1Box.fadeIn(300);
+    $attack2Box.fadeIn(400);
+    $attack3Box.fadeIn(500);
+    $attack4Box.fadeIn(600);
+  }, 1000);
+});
 
-  //I. Player Turn Function
-    var attack = function() {       // NEEDS EVENT LISTENER ON ATTACK NAME'S CLICK
-      //a. damage calculation fnc
-      activeAvatar.battle(activeEnemyAvatar);
+//THE MASTER FUNCTION Clicking an Attack Box initiates attack fnc with specific move. Adds animate transition.
+for (i=0;i<4;i++) {
+  $attackBoxArray[i].on("click", masterAttackFNC() {
+    activeAvatar.battle(activeAvatar.attackPowers[activeAvatar.attackNames.indexOf($(this).text())]);
+    console.log(queryAttack);
+    console.log(activeAvatar);
+    console.log(activeEnemyAvatar);
+    console.log(activeEnemyAvatar.currenthealth);
+  })
+};
       //b. "declares attack in text"
 
       //c. "attack animation"
@@ -102,7 +147,7 @@ console.log("POKEBALLS READY");
       //e. "health bar goes down"
 
       //f. "attack effectiveness/oneliner in text"
-
+var somefunc = function() {
       //g1. enemy's turn fnc (uses while hp>0, else...)
       if (activeEnemyAvatar.currenthealth > 0) {
         enemyAttack(activeEnemyAvatar, activeAvatar);
@@ -170,45 +215,32 @@ console.log("POKEBALLS READY");
     //h. keith one-liner
     //i. enemy turn fnc (uses while hp>0)
 
-  var switchList = function() {                                                             //NEEDS TO TURN TO CLICK EVENT ON Avatar UL LIST.
-      console.log(" Your Active Avatar is: " + activeAvatar.name);
-      console.log("Currently in slots:")
-      for (i=0; i<usableAvatarList.length; i++) {
-        console.log(usableAvatarList[i].name);
-        console.log("Current Health: " + usableAvatarList[i].currenthealth);
-      }
-      activeAvatar = usableAvatarList[0];
-      console.log("Switched out to " + activeAvatar.name)
-  };
-
-
-  //2. Enemy turn fnc:
-    //I. attack (random if more than one attack)
-
-      //a. damage calculation fnc
-      //b. "declares attack in text"
-      //c. "attack animation"
-      //d. "response animation"
-      //e. "health bar reacts"
-      //f. "attack effectiveness/oneliner in text"
-      //g1. player turn fnc. (uses while hp > 0, else...)
-      //g2. end battle fnc.
-
-    //II.
-
-var $worldMap = $('#world_map');
-var $battleBoard = $('#battle_board');
-
 var subRenderBattleScreen = function() {
-  $worldMap.css({"z-index": "1", "visibility": "hidden"});
+  renderActiveAttacks();
+  $worldMap.fadeOut(1000);
   setTimeout(function() {
-  $battleBoard.css({"z-index": "3", "visibility": "visible"});
-}, 2000);
+    $battleBoard.toggle(800);
+    $battleBoard.fadeIn(800);
+  },1000);
+  setTimeout(function() {
+    $( "#enemy_info" ).animate({
+    left: "+=150", },1000);
+    $( "#player_info" ).animate({
+    right: "+=150", },1000);
+  }, 1100);
+  setTimeout(function() {
+    $fightTextBox.fadeIn(1000);
+    $itemTextBox.fadeIn(1600);
+    $famTextBox.fadeIn(2200);
+    $boneOutTextBox.fadeIn(2400);
+  },2000);
 };
 
 var subRenderWorldScreen = function() {
-  $worldMap.css({"z-index": "3", "visibility": "visible"});
-  $battleBoard.css({"z-index": "1", "visibility": "hidden"});
+    $battleBoard.fadeOut(1000);
+    setTimeout(function() {
+      $worldMap.fadeIn(800);
+    },1000);
 };
 
 
